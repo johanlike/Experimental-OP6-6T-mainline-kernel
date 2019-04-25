ae9d9d0440e5c7a2adbc45e4be3ee985d24f0d56
Sami Tolvanen <samitolvanen@google.com>
2019-04-25 16:09:05 -0700

    ANDROID: add support for Clang's Control Flow Integrity (CFI)
    This change adds the CONFIG_CFI_CLANG option, CFI error handling,
    and a faster look-up table for cross module CFI checks.
    
    Bug: 145210207
    Change-Id: I118303de50114ca6f85d89a7d69c5cbc47e2f5c0
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    Signed-off-by: Matthias Maennich <maennich@google.com>

--- include/linux/compiler_types.h
+++ include/linux/compiler_types.h
@@ -201,6 +201,10 @@ struct ftrace_likely_data {
 # define __norecordmcount
 #endif
 
+#ifndef __nocfi
+# define __nocfi
+#endif
+
 #ifndef asm_volatile_goto
 #define asm_volatile_goto(x...) asm goto(x)
 #endif
