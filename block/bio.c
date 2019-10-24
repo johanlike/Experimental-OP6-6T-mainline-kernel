825723e6ce5916a192c25dfd5a0df9c59670df82
Satya Tangirala <satyat@google.com>
2019-10-24 14:44:25 -0700

    FROMLIST: block: blk-crypto for Inline Encryption
    We introduce blk-crypto, which manages programming keyslots for struct
    bios. With blk-crypto, filesystems only need to call bio_crypt_set_ctx with
    the encryption key, algorithm and data_unit_num; they don't have to worry
    about getting a keyslot for each encryption context, as blk-crypto handles
    that. Blk-crypto also makes it possible for layered devices like device
    mapper to make use of inline encryption hardware.
    
    Blk-crypto delegates crypto operations to inline encryption hardware when
    available, and also contains a software fallback to the kernel crypto API.
    For more details, refer to Documentation/block/inline-encryption.rst.
    
    Bug: 137270441
    Test: tested as series; see Ie1b77f7615d6a7a60fdc9105c7ab2200d17636a8
    Change-Id: I7df59fef0c1e90043b1899c5a95973e23afac0c5
    Signed-off-by: Satya Tangirala <satyat@google.com>
    Link: https://patchwork.kernel.org/patch/11214731/
    Signed-off-by: Matthias Maennich <maennich@google.com>

--- block/bio.c
+++ block/bio.c
@@ -18,6 +18,7 @@
 #include <linux/blk-cgroup.h>
 #include <linux/highmem.h>
 #include <linux/sched/sysctl.h>
+#include <linux/blk-crypto.h>
 
 #include <trace/events/block.h>
 #include "blk.h"
@@ -1392,6 +1393,10 @@ void bio_endio(struct bio *bio)
 again:
 	if (!bio_remaining_done(bio))
 		return;
+
+	if (!blk_crypto_endio(bio))
+		return;
+
 	if (!bio_integrity_endio(bio))
 		return;
 
