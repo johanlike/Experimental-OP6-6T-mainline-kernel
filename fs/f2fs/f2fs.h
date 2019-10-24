e900a0c2a5303195034482e60d49306a82c41b43
Satya Tangirala <satyat@google.com>
2019-10-24 14:44:30 -0700

    FROMLIST: f2fs: add inline encryption support
    Wire up f2fs to support inline encryption via the helper functions which
    fs/crypto/ now provides.  This includes:
    
    - Adding a mount option 'inlinecrypt' which enables inline encryption
      on encrypted files where it can be used.
    
    - Setting the bio_crypt_ctx on bios that will be submitted to an
      inline-encrypted file.
    
    - Not adding logically discontiguous data to bios that will be submitted
      to an inline-encrypted file.
    
    - Not doing filesystem-layer crypto on inline-encrypted files.
    
    Bug: 137270441
    Test: tested as series; see Ie1b77f7615d6a7a60fdc9105c7ab2200d17636a8
    Change-Id: I2ea4da9e251e668b908408ecc091f09ebf8be8f4
    Co-developed-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Satya Tangirala <satyat@google.com>
    Link: https://patchwork.kernel.org/patch/11214785/
    Signed-off-by: Matthias Maennich <maennich@google.com>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -139,6 +139,9 @@ struct f2fs_mount_info {
 	int fs_mode;			/* fs mode: LFS or ADAPTIVE */
 	int bggc_mode;			/* bggc mode: off, on or sync */
 	bool test_dummy_encryption;	/* test dummy encryption */
+#ifdef CONFIG_FS_ENCRYPTION
+	bool inlinecrypt;		/* inline encryption enabled */
+#endif
 	block_t unusable_cap;		/* Amount of space allowed to be
 					 * unusable when disabling checkpoint
 					 */
